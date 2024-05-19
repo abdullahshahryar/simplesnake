75309cf07b66367864d67dcb6ca396d8e6f7ded3
abdullahshahryar <abdullahshahryar56@gmail.com>
2024-05-19 01:28:58 -0700

    DAY 3
    finish

--- SNAKE.cpp
+++ SNAKE.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <conio.h>
+#include <thread> // for std::this_thread::sleep_for
+#include <chrono> // for std::chrono::milliseconds
+#include <windows.h>
 
 using namespace std;
 
@@ -9,6 +12,8 @@ const int height = 20;
 int x, y, fruitX, fruitY, Score;
 enum eDirection { STOP=0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
+int tailX[100], tailY[100];
+int ntail;
 
 void setup()
 {
@@ -18,17 +23,22 @@ void setup()
     y = height / 2;
     fruitX = rand() % width;
     fruitY = rand() % height;
+
     Score = 0;
 
 }
 void draw()
 {
-    system("cls");
-    for (int i = 0; i < width; i++)
-    cout << "#";
+    // Clear the screen
+    SetConsoleCursorPosition
+    (GetStdHandle(STD_OUTPUT_HANDLE), {0,0});
+
+    // Draw the game board
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
     cout << endl;
 
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
@@ -39,27 +49,40 @@ void draw()
             else if (i == fruitY && j == fruitX)
                 cout << "F";
             else
-                cout << " ";
-            if (j == width-1)
+            {
+                bool print = false;
+                for (int k = 0; k < ntail; k++)
+                {
+                    
+                    if (tailX[k] == j &&  tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                    
+                }
+
+             if (!print)
+              cout << " ";
+            }
+                
+            if (j == width - 1)
                 cout << "#";
-
         }
-
         cout << endl;
     }
-    
-    for (int i = 0; i < width+2; i++)
-    cout << "#";
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
     cout << endl;
     cout << "SCORE" << Score << endl;
-
 
 }
 
 void input()
 {
-
-    if (_kbhit())
+    
+       if (_kbhit())
     {
         switch (_getch())
         {
@@ -79,12 +102,27 @@ void input()
             gameOver = true;
             break;  
         }
-    }
-
+    }  
+    
 }
 void logic()
 {
+    int pX = tailX[0];
+    int pY = tailY[0];
+    int p2X, p2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < ntail; i++)
+    {
+        p2X = tailX[i];
+        p2Y = tailY[i];
+        tailX[i] = pX;
+        tailY[i] = pY;
+        pX = p2X;
+        pY = p2Y;
+    }
     switch (dir)
+
    {
         case LEFT:
            x--;
@@ -98,17 +136,22 @@ void logic()
         case DOWN:
            y++;
            break;
-
+        case STOP:
+           break;
    }
 
    if (x > width || x < 0 || y > height || y < 0)
    gameOver = true;
-
+   
+   for (int i = 0; i < ntail; i++)
+   if (tailX[i] == x && tailY[i] == y)
+   gameOver = true;
    if (x == fruitX && y == fruitY)
    {
         Score +=10;
         fruitX = rand() % width;
         fruitY = rand() % height;
+        ntail++;
    }
 }
 
@@ -118,8 +161,10 @@ int main()
     while (!gameOver)
     {
         draw();
-        input();
         logic();
+        input();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(90));
         
     }
 
